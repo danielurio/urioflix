f8b37412f5bcc6a60014086f99b1f8c00b57b81b
Urio <danielurio@gmail.com>
2020-07-30 17:54:13 +0200

    Adding FormField

--- src/pages/register/Category/index.js
+++ src/pages/register/Category/index.js
@@ -1,15 +1,63 @@
-import React from "react";
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
 import PageDefault from "../../../components/PageDefault";
+import FormField from "../../../components/FormField";
 
 function RegisterCategory() {
+  let defaultValues = {
+    name: "",
+    description: "",
+    color: "",
+  };
+
+  const [categories, setCategories] = useState([]);
+  const [values, setValues] = useState(defaultValues);
+
+  function setValue(key, value) {
+    setValues({ ...values, [key]: value });
+  }
+  function handleChange(info) {
+    const { name, value } = info.target;
+    setValue(name, value);
+  }
+
   return (
-      <PageDefault>
-        <h1>Register Category</h1>
-        <Link to="/">
-          home
-        </Link>
-      </PageDefault>
+    <PageDefault>
+      <h1>Register Category:{values.name}</h1>
+      <form
+        onSubmit={(info) => {
+          info.preventDefault();
+          setCategories([...categories, values]);
+          setValues(defaultValues);
+        }}
+      >
+        <FormField
+          label="Category Name:"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Category Description:"
+          name="description"
+          value={values.description}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Color:"
+          type="color"
+          name="color"
+          value={values.color}
+          onChange={handleChange}
+        />
+        <button>Confirm</button>
+      </form>
+      <ul>
+        {categories.map((category, index) => {
+          return <li key={index}>{category.name}</li>;
+        })}
+      </ul>
+    </PageDefault>
   );
 }
 
