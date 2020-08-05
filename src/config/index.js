9d31b31873ba8a978ddff809691bce18d17fac6e
Daniel Urio <danielurio@gmail.com>
2020-08-05 19:36:30 +0200

    Fix config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const URL_API = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://juntos-api.herokuapp.com';
+  : 'https://urioflix-json-server.herokuapp.com';
 
 export default { URL_API };
